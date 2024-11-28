e76d9876a63aeca26ec3349bea1921713cdbbc96
9034sonu <52558804+9034sonu@users.noreply.github.com>
2024-11-28 23:07:12 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.html', './src/**/*.ts'],
+  },
+}
+
